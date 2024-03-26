d7aa03e437f9d6563fd697b87c04a2a7b0a1a10e
HrisyToteva <hristiqnatoteva9718@gmail.com>
2024-03-26 13:11:42 +0100

    create capability command (only required fields)

--- cmds/capability/capability.go
+++ cmds/capability/capability.go
@@ -2,6 +2,7 @@ package capability
 
 import (
 	"fmt"
+	"go.dfds.cloud/ticli/cmds/configuration"
 	"os"
 
 	"go.dfds.cloud/ticli/selfservice"
@@ -11,6 +12,9 @@ import (
 
 var (
 	selfserviceClient *selfservice.SelfServiceClient
+
+	description string
+	metadata    string
 )
 
 var CapabilityCmd = &cobra.Command{
@@ -24,6 +28,14 @@ var CapabilityCmd = &cobra.Command{
 func InitializeCapability(accessToken string) {
 	CapabilityCmd.AddCommand(queryCmd)
 	CapabilityCmd.AddCommand(capabilityByIdCmd)
+	b := createCapabilityCmd
+	CapabilityCmd.AddCommand(b)
+
+	createCapabilityCmd.PersistentFlags().StringVar(&description, "description", "", "adds a description to a capability (required)")
+	configuration.BindFlag("description", createCapabilityCmd.PersistentFlags().Lookup("description"))
+	//createCapabilityCmd.MarkFlagRequired("description")
+	createCapabilityCmd.PersistentFlags().StringVar(&metadata, "metadata", "", "add matadata JSON to a capability")
+	configuration.BindFlag("metadata", createCapabilityCmd.PersistentFlags().Lookup("metadata"))
 
 	selfserviceClient = selfservice.NewSelfServiceClient(accessToken)
 }
@@ -47,6 +59,27 @@ var capabilityByIdCmd = &cobra.Command{
 		}
 		capabilityById := selfserviceClient.GetCapabilityByID(args[0])
 		fmt.Println(capabilityById)
+
+	},
+}
+
+var createCapabilityCmd = &cobra.Command{
+	Use:   "create [NAME]",
+	Short: "creates a new capability",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("Missing id")
+			os.Exit(1)
+		}
+		capabilityStruct := selfservice.CapabilityRequest{
+			Name:         args[0],
+			Description:  description,
+			Invitees:     []string{},
+			JsonMetadata: metadata,
+		}
+		fmt.Println(capabilityStruct)
+		capability := selfserviceClient.CreateCapability(capabilityStruct)
+		fmt.Println(capability)
 
 	},
 }
